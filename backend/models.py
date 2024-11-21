e843f1e1ecdc236241652082a2d3d1637ae08f24
gurpreetgit2 <gurpreet2b@gmail.com>
2024-11-21 23:18:25 +0530

    Ran_first_build_success

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-database_name = 'trivia'
-database_user = 'postgres'
-database_password = 'password'
-database_host = 'localhost:5432'
-database_path = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
+
+database_name = "trivia"
+database_user = "gurpreetatwal"
+database_password = "abc"
+database_host = "localhost:5432"
+database_path = (
+    f"postgresql://{database_user}:{database_password}@{database_host}/{database_name}"
+)
 
 db = SQLAlchemy()
 
@@ -12,16 +15,21 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
+
+
 def setup_db(app, database_path=database_path):
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
 
 """
 Question
 """
+
+
 class Question(db.Model):
-    __tablename__ = 'questions'
+    __tablename__ = "questions"
 
     id = Column(Integer, primary_key=True)
     question = Column(String, nullable=False)
@@ -48,18 +56,21 @@ class Question(db.Model):
 
     def format(self):
         return {
-            'id': self.id,
-            'question': self.question,
-            'answer': self.answer,
-            'category': self.category,
-            'difficulty': self.difficulty
+            "id": self.id,
+            "question": self.question,
+            "answer": self.answer,
+            "category": self.category,
+            "difficulty": self.difficulty,
         }
+
 
 """
 Category
 """
+
+
 class Category(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True)
     type = Column(String, nullable=False)
@@ -68,7 +79,4 @@ class Category(db.Model):
         self.type = type
 
     def format(self):
-        return {
-            'id': self.id,
-            'type': self.type
-        }
+        return {"id": self.id, "type": self.type}
